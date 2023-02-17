e9c87762346c4d1b8ffcfeaea72fef4e53e98899
dimas <kabanooovd@yandex.ru>
2023-02-17 10:10:30 +0300

    #1

---
+++ config/build/esbuild-config.js
@@ -0,0 +1,20 @@
+const ESBuild = require("esbuild")
+const path = require("path")
+
+const MODES = {
+  dev: "development",
+  prod: "production",
+}
+
+const mode = process.env.MODE || MODES.dev;
+
+const isDev = mode === MODES.dev;
+const isProd = mode === MODES.prod;
+
+ESBuild.build({
+  outdir: path.resolve(__dirname, "..", "..", "dist"),
+  entryPoints: [path.resolve(__dirname, "..", "..", "src", "index.js")],
+  entryNames: "bundle",
+  bundle: true,
+  minify: isProd,
+})
---
+++ src/test.js
@@ -0,0 +1,11 @@
+export const foo = () => {
+  return "TEEEEST"
+}
+
+const a = 5
+
+const b = 15
+
+export const foo2 = () => {
+  return "123"
+}
